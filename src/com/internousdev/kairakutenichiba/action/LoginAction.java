8a7f3d7b9f2c10b88bb7000a082ad55c0b2a7430
ryohei kato <urimoke@gmail.com>
2017-09-04 16:21:26 +0900

    loginaction修正

--- src/com/internousdev/kairakutenichiba/action/LoginAction.java
+++ src/com/internousdev/kairakutenichiba/action/LoginAction.java
@@ -17,18 +17,116 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class LoginAction extends ActionSupport implements SessionAware {
 
-	private Map<String,Object> session;
-
-	private String userid;
+	/**
+	 * メールアドレス
+	 */
+	private String email;
+	/**
+	 * パスワード
+	 */
 	private String password;
+	/**
+	 * セッション情報（ユーザーID）
+	 */
+	private Map<String, Object> session;
 
 
+
+
+
+	/**
+	 * ログインするための実行メソッド
+	 *
+	 * @return ログイン成功 =SUCCESS 失敗 =ERROR 管理人ならLOGIN
+	 */
 	public String execute() {
 		String ret = ERROR;
 
 		LoginDAO dao = new LoginDAO();
 		LoginDTO dto = new LoginDTO();
-		dto = dao.select(userid, password);
-}
+		dto = dao.select(email, password);
+
+		if (email.equals(dto.getEmail())) {
+			if (password.equals(dto.getPassword())) {
+
+				if (dto.isLoginFlg() == false) {
+
+					if (dao.update(dto.getEmail(), dto.getPassword()) > 0) {
+
+						dto = dao.select(dto.getEmail(), dto.getPassword());
+
+						session.put("userId", dto.getUserId());
+						session.put("loginFlg", dto.isLoginFlg());
+
+
+
+
+					}
+				}
+			}
+		}
+
+		return ret;
+
+	}
+
+	/**
+	 * メールアドレスを取得するメソッド
+	 *
+	 * @return email メールアドレス
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * メールアドレスを格納するメソッド
+	 *
+	 * @param Email
+	 *            セットする
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * パスワードを取得するメソッド
+	 *
+	 * @return password パスワード
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * パスワードを格納するメソッド
+	 *
+	 * @param password
+	 *            セットする
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * セッション情報を取得するメソッド
+	 *
+	 * @return session セッション情報
+	 */
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	/**
+	 * セッション情報を格納するメソッド
+	 *
+	 * @param session
+	 *            セットする session
+	 */
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+
 
 }
