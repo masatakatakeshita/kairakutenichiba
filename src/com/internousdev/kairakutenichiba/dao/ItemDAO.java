1d8549f6a7bec2bfbef32fc6dfda9e9fcb582574
hayato shoji <shojihayato1618@gmail.com>
2017-09-11 14:29:41 +0900

    ItemDAOに×ついたからいじったけど、これたぶん私たちのサイトには要らないですね。荘司

--- src/com/internousdev/kairakutenichiba/dao/ItemDAO.java
+++ src/com/internousdev/kairakutenichiba/dao/ItemDAO.java
@@ -19,15 +19,13 @@ import com.internousdev.util.DBConnector;
 public class ItemDAO {
 
 	/**
-	 * 野菜の種類を取得しリストに格納するメソッド
-	 * @return Item アイテム情報
-	 * @version 1.0
+	 *
 	 */
        public ArrayList<ItemDTO> select(){
        	DBConnector db = new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/", "openconnect", "root","mysql");
        	Connection con = db.getConnection();
        	ArrayList<ItemDTO> Item = new ArrayList<ItemDTO>();
-       	String sql ="select * from sorts";
+       	String sql ="select * from items";
 
 
        	try{
@@ -35,9 +33,9 @@ public class ItemDAO {
     		ResultSet rs = ps.executeQuery();
     		while(rs.next()){
     			ItemDTO dto = new ItemDTO();;
-    			dto.setItemGenre(rs.getString("sorts")); //種類
-    			dto.setCategory_id(rs.getInt("sort_id")); //種類ID
-    			dto.setImgPath(rs.getString("sort_img")); //イメージパス
+    			
+    			dto.setCategory(rs.getString("category")); //種類ID
+    		
     			Item.add(dto);
        		}
        	}catch(SQLException e){
