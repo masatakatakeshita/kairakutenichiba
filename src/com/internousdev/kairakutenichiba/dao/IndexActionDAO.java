2f3d1972af0f25479503735a85bdfd9024048a44
Masataka.Takeshita <takeshita319@gmail.com>
2017-09-25 16:45:19 +0900

    index.jsp修正 IndexAction IndexActionDAO追加

---
+++ src/com/internousdev/kairakutenichiba/dao/IndexActionDAO.java
@@ -0,0 +1,102 @@
+/**
+ *
+ */
+package com.internousdev.kairakutenichiba.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.internousdev.kairakutenichiba.dto.ItemDTO;
+import com.internousdev.kairakutenichiba.util.DBConnector;
+
+/**
+ * @author internous
+ *
+ */
+public class IndexActionDAO  {
+
+	ArrayList<ItemDTO> itemList=new ArrayList<ItemDTO>();
+
+	public ArrayList<ItemDTO> select(){
+		DBConnector db=new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/", "kairakutenichiba", "root","mysql");
+		Connection con=db.getConnection();
+
+		String sql="select * from items";
+
+		try{
+			PreparedStatement ps= con.prepareStatement(sql);
+
+			ResultSet rs=ps.executeQuery();
+
+			while (rs.next()) {
+				ItemDTO dto= new ItemDTO();
+
+				dto.setItem_id(rs.getInt("item_id"));
+
+				dto.setCategory(rs.getString("category"));
+
+				dto.setItem_image(rs.getString("item_image"));
+
+				itemList.add(dto);
+			}
+
+			rs.close();
+			ps.close();
+			con.close();
+
+
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+
+
+
+		return itemList;
+	}
+
+
+
+
+
+
+	public ArrayList<ItemDTO> select(String category){
+		DBConnector db=new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/", "openconnect", "root","mysql");
+		Connection con=db.getConnection();
+
+		String sql="select * from items where category = ?";
+
+		try{
+			PreparedStatement ps= con.prepareStatement(sql);
+            ps.setString(1,category);
+			ResultSet rs=ps.executeQuery();
+
+			while (rs.next()) {
+				ItemDTO dto= new ItemDTO();
+
+				dto.setItem_id(rs.getInt("item_id"));
+
+				dto.setCategory(rs.getString("category"));
+
+				dto.setItem_image(rs.getString("item_image"));
+
+				itemList.add(dto);
+			}
+
+			rs.close();
+			ps.close();
+			con.close();
+
+
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+
+
+
+		return itemList;
+	}
+
+}
